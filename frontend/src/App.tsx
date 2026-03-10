219171081dc329d03fa6dec1760364309a001311
devcarlosperez <carlosperezsantana04@gmail.com>
2026-03-10 10:04:20 +0000

    Add /create-event route to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Header } from './Components/Header/Header'
 import { Footer } from './Components/Footer/Footer'
 import { Login } from './Pages/Login/Login'
-import{About} from './Pages/About/About'
+import { About } from './Pages/About/About'
+import { CreateEvent } from './Pages/CreateEvent/CreateEvent'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/About" element={<About />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/create-event" element={<CreateEvent />} />
       </Routes>
       <Footer />
     </BrowserRouter>
